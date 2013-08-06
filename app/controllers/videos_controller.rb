c658f7066d2cf120def27172d0e72382d85d5d4f
Josh <jscanish@gmail.com>
2013-08-06 04:59:49 -0400

    adjusted category index page and controller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,9 +1,5 @@
 class VideosController < ApplicationController
 
-  def index
-    @videos = Video.all
-  end
-
   def show
     @video = Video.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Myflix::Application.routes.draw do
  #get "/home", to: "videos#index"
   #get "/home", to: "videos#index"
   #get "/videos/:id", to: "videos#show", as: 'video_path'
-  resources :videos, only: [:show, :index]
-  resources :categories, only: [:show]
+  resources :videos, only: [:show]
+  resources :categories, only: [:show, :index]
 end
