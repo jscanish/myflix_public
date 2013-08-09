3173ba074fcef59b6c58266bb6ad28a118fb237d
Josh <jscanish@gmail.com>
2013-08-09 02:21:35 -0400

    adjusted home url

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
 
- #get "/home", to: "videos#index"
-  #get "/home", to: "videos#index"
-  #get "/videos/:id", to: "videos#show", as: 'video_path'
+  get "/home", to: "categories#index"
   resources :videos, only: [:show]
-  resources :categories, only: [:show, :index]
+  resources :categories, only: [:show]
 end
