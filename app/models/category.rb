0e8b00d0e3e3ca3ca51cf3c82b8ba418c2499be7
Josh <jscanish@gmail.com>
2013-08-09 23:11:59 -0400

    added recent_videos method and tests

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ActiveRecord::Base
   has_many :videos
+
+  def recent_videos
+    self.videos.last(6).reverse
+  end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,4 +2,18 @@ require 'spec_helper'
 
 describe Category do
   it {should have_many(:videos)}
+
+  describe "#recent_videos" do
+    it "shows all videos if total < 6" do
+      Video.create(title: "Monk", description: "a good show", category_id: 1)
+      Video.create(title: "Sherlock", description: "a great show", category_id: 1)
+      Category.create(name: "Comedy")
+      expect(Category.find(1).recent_videos).to eq([Video.find(2), Video.first])
+    end
+    it "should show only six videos most recent videos" do
+      8.times{Video.create(title: "Monk", description: "a good show", category_id: 1)}
+      Category.create(name: "Drama")
+      expect(Category.find(1).recent_videos).to eq([Video.find(8), Video.find(7), Video.find(6), Video.find(5), Video.find(4), Video.find(3)])
+    end
+  end
 end
