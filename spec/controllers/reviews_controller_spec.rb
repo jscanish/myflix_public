e6d6ad7d8fc116846a3de4c13df4c93a7b4037cd
Josh <jscanish@gmail.com>
2013-08-14 23:39:41 -0400

    added reviews and review tests

--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -1,4 +1,59 @@
 require 'spec_helper'
 
 describe ReviewsController do
+  describe "POST create" do
+    let(:video) { Fabricate(:video) }
+
+    context "with authenticated users" do
+      let(:current_user) { Fabricate(:user) }
+      before { session[:user_id] = current_user.id}
+
+      context "with valid credentials" do
+        before do
+          post :create, review: Fabricate.attributes_for(:review), video_id: video.id
+        end
+        it "redirects to current page" do
+          expect(response).to redirect_to video
+        end
+        it "creates the review" do
+          expect(Review.count).to eq(1)
+        end
+        it "associates review with proper video" do
+          expect(Review.first.video).to eq(video)
+        end
+        it "associates review with current_user" do
+          expect(Review.first.user).to eq(current_user)
+        end
+        it "flashs success message" do
+          expect(flash[:notice]).to_not be_blank
+        end
+      end
+
+      context "with invalid credentials" do
+        it "does not create review" do
+          post :create, review: Fabricate.attributes_for(:review, content: nil), video_id: video.id
+          expect(video.reviews.count).to eq(0)
+        end
+        it "renders the video show template" do
+          post :create, review: Fabricate.attributes_for(:review, content: nil), video_id: video.id
+          expect(response).to render_template "videos/show"
+        end
+        it "sets @video variable" do
+          post :create, review: Fabricate.attributes_for(:review, content: nil), video_id: video.id
+          expect(assigns(:video)).to eq(video)
+        end
+        it "sets @review variable" do
+          review = Fabricate(:review, video: video)
+          post :create, review: {rating: 4}, video_id: video.id
+          expect(assigns(:failed_review)).to match_array([review])
+        end
+      end
+    end
+    context "with unauthenticated users" do
+      it "redirects to home page" do
+        post :create, review: Fabricate.attributes_for(:review), video_id: video.id
+        expect(response).to redirect_to login_path
+      end
+    end
+  end
 end
