549bec5ea0a2cbe1e98f9c5b47968efe0b3548d6
Josh <jscanish@gmail.com>
2013-08-16 19:30:55 -0400

    added rating to my_queue

--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -1,4 +1,13 @@
 class QueueItem < ActiveRecord::Base
   belongs_to :user
   belongs_to :video
+
+  def review_rating
+    @review = Review.where(user_id: user_id, video_id: video_id).first
+    if @review != nil
+      @review.rating
+    else
+      nil
+    end
+  end
 end
