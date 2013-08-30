b85ae1fce64ced167018a40cf4c72d59cb9a57e8
Josh <jscanish@gmail.com>
2013-08-30 16:46:24 -0400

    category controller tests

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
   has_many :videos
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 
   def recent_videos
     videos.last(6).reverse
