0ed043a354d8d825fcf6e05e652e86da5f2ced61
Josh <jscanish@gmail.com>
2013-09-07 00:16:14 -0500

    deploy to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,8 +70,8 @@ Myflix::Application.configure do
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'example.com',
-  user_name:            '<username>',
-  password:             '<password>',
+  user_name:            ENV['gmail_username']
+  password:             ENV['gmail_password']
   authentication:       'plain',
   enable_starttls_auto: true  }
 end
