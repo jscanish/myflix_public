3078bf0be265d0249d133594b88683da40edf1eb
Josh <jscanish@gmail.com>
2013-09-10 16:19:53 -0500

    trouble with feature test

--- spec/features/user_resets_password_spec.rb
+++ spec/features/user_resets_password_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 feature "user resets password" do
   scenario "user resets password by following email link" do
     user = Fabricate(:user)
-
+    clear_emails
     visit forgot_password_path
-    page.should have_content "We will send you an email with a link that you can use to "
+    expect(page).to have_content("We will send you an email with a link that you can use to ")
     fill_in "email", with: user.email
     click_button "Send Email"
-    page.should have_content "We have sent an email with instructions to reset your password"
+    expect(page).to have_content("We have sent an email with instructions to reset your password")
 
   end
 end
