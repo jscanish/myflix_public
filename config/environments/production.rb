9c027de40fa1ed7342ac6ef71f85daf8359c384a
Josh <jscanish@gmail.com>
2013-09-15 00:24:11 -0500

    minor change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,15 +65,6 @@ Myflix::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  # address:              'smtp.gmail.com',
-  # port:                 587,
-  # domain:               'http://safe-hamlet-6689.herokuapp.com/',
-  # user_name:            ENV['gmail_username'],
-  # password:             ENV['gmail_password'],
-  # authentication:       'plain',
-  # enable_starttls_auto: true  }
 
   config.action_mailer.smtp_settings = {
   :port           => ENV['MAILGUN_SMTP_PORT'],
